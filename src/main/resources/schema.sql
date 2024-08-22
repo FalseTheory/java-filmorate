f9127e009f3c17a5232c3a3c0bbfa4a1bfb8b0c9
Fairgraves <rozaco0@gmail.com>
2024-08-22 13:29:32 +0700

    <feature> Создать соединение с базой данный, создать схему

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,63 @@
+CREATE TABLE IF NOT EXISTS films (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	"description" varchar(200) NOT NULL,
+	"release_date" date NOT NULL,
+	"duration" bigint NOT NULL,
+	"rating" bigint NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS users (
+	"id" serial NOT NULL UNIQUE,
+	"email" varchar(255) NOT NULL UNIQUE,
+	"login" bigint NOT NULL,
+	"name" bigint,
+	"birthday" date NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS film_genres (
+	"film_id" serial NOT NULL UNIQUE,
+	"genre_id" bigint NOT NULL,
+	PRIMARY KEY ("film_id")
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL UNIQUE,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS mpa_rating (
+	"rating_id" serial NOT NULL UNIQUE,
+	"name" bigint NOT NULL UNIQUE,
+	PRIMARY KEY ("rating_id")
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+	"user_id" bigint NOT NULL UNIQUE,
+	"friend_id" bigint NOT NULL UNIQUE,
+	PRIMARY KEY ("user_id", "friend_id")
+);
+
+CREATE TABLE IF NOT EXISTS film_likes (
+	"film_id" bigint NOT NULL,
+	"user_id" bigint NOT NULL,
+	PRIMARY KEY ("film_id")
+);
+
+ALTER TABLE films ADD CONSTRAINT IF NOT EXISTS "films_fk5" FOREIGN KEY ("rating") REFERENCES mpa_rating("rating_id");
+
+
+ALTER TABLE film_genres ADD CONSTRAINT IF NOT EXISTS "film_genres_fk0" FOREIGN KEY ("film_id") REFERENCES films("id");
+
+ALTER TABLE film_genres ADD CONSTRAINT IF NOT EXISTS "film_genres_fk1" FOREIGN KEY ("genre_id") REFERENCES genres("id");
+
+
+ALTER TABLE friends ADD CONSTRAINT IF NOT EXISTS "friends_fk0" FOREIGN KEY ("user_id") REFERENCES users("id");
+
+ALTER TABLE friends ADD CONSTRAINT IF NOT EXISTS "friends_fk1" FOREIGN KEY ("friend_id") REFERENCES users("id");
+ALTER TABLE film_likes ADD CONSTRAINT IF NOT EXISTS "film_likes_fk0" FOREIGN KEY ("film_id") REFERENCES films("id");
+
+ALTER TABLE film_likes ADD CONSTRAINT IF NOT EXISTS "film_likes_fk1" FOREIGN KEY ("user_id") REFERENCES users("id");
