0be28c1da15cf37d15ed8363be5d048dedeb9c17
Fairgraves <rozaco0@gmail.com>
2024-08-26 01:31:07 +0700

    <fix> fix test-data.sql

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -7,6 +7,9 @@ VALUES('gmail@mail.com', 'user2', 'name2', '2001-01-20');
 INSERT INTO MPA_RATING ("mpa_name")
 VALUES('PG');
 
+INSERT INTO MPA_RATING ("mpa_name")
+VALUES('PG-13');
+
 INSERT INTO FILMS ("name","description","release_date","duration","rating")
 VALUES('film','description','2012-01-20',10,1);
 
@@ -23,13 +26,11 @@ INSERT INTO FILM_GENRES ("film_id", "genre")
 VALUES(1,2);
 
 INSERT INTO FILMS ("name","description","release_date","duration","rating")
-VALUES('film2','description2','2012-01-21',20,1);
+VALUES('film2','description2','2012-01-21',20,2);
 
 INSERT INTO FILM_GENRES ("film_id", "genre")
 VALUES(2,1);
 
-INSERT INTO MPA_RATING ("mpa_name")
-VALUES('PG-13');
 
 INSERT INTO FILM_LIKES ("film_id", "user_id") VALUES(1, 1);
 INSERT INTO FILM_LIKES ("film_id", "user_id") VALUES(2, 1);
